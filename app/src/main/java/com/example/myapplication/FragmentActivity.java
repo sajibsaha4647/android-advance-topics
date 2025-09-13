382d02c95f2eacceeb3da517435e93fd30f3aa66
sajibsaha4647 <sksaha4648@gmail.com>
2025-09-14 02:13:22 +0600

    updated with fragment activity

--- app/src/main/java/com/example/myapplication/FragmentActivity.java
+++ app/src/main/java/com/example/myapplication/FragmentActivity.java
@@ -1,13 +1,22 @@
 package com.example.myapplication;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import  android.app.Fragment;
 
+import com.example.myapplication.Fragments.AddressFragment;
+import com.example.myapplication.Fragments.NameFragment;
 import com.example.myapplication.databinding.ActivityFragmentBinding;
 
 
@@ -26,16 +35,41 @@ public class FragmentActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
         setTitle("Fragment view");
-
         // Enable the back/up button in the toolbar
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-//            getSupportActionBar().setHomeAsUpIndicator(true); // optional custom icon
         }
+
+
+
+
+        String[] fruits = {"Mango","Orange","Apple","Papia","Goyaba","Dalim","Wanter menol"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,fruits);
+            binding.ListviewId.setAdapter(adapter);
+
+        binding.ListviewId.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fragment fragment;
+
+                if (position == 0) {
+                    fragment = new AddressFragment();
+                } else if (position == 1) {
+                    fragment = new NameFragment();
+                } else {
+                    fragment = new NameFragment();
+                }
+
+                FragmentManager fragmentManager = getFragmentManager(); // ✅ use support manager
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragId, fragment);
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 
     @Override
